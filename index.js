48c988079580d99961243e6757f63e2c54471dad
Riza Fahmi <rizafahmi@gmail.com>
2016-04-19 21:05:41 +0700

    Adding mongodb

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ import express from 'express'
 import schema from './data/schema.js'
 import GraphQLHTTP from 'express-graphql'
 
+var MongoClient = require('mongodb').MongoClient
+
 let app = express()
 
-app.get('/', (req, res) => {
-  res.send("Hello, program")
+let db
+let url = 'mongodb://localhost:27017/linky'
+
+MongoClient.connect(url, (err, database) => {
+  if (err) throw err
+
+  db = database
+  app.get('/', (req, res) => {
+    res.send("Hello, program")
+  })
+
+  app.use('/graphql', GraphQLHTTP({
+    schema,
+    graphiql: true
+  }))
+
+  app.listen(3000, () => {
+    console.log("Server running on port 3000!")
+  })
+
 })
 
-app.use('/graphql', GraphQLHTTP({
-  schema,
-  graphiql: true
-}))
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000!")
-})
